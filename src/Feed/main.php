4b420c6c83555c1a0f0a6b962701f81bfcaee421
NewTheBuilder <61889427+NewtheBuilder@users.noreply.github.com>
2020-04-02 14:17:57 +0200

    Add files via upload

---
+++ src/Feed/main.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Feed;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+class main extends PluginBase implements Listener {
+
+
+    public function onEnable() {
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+
+        if ($sender instanceof Player){
+
+        if ($sender->getName() == "feed") {
+
+            $sender->sendMessage("§aTu as bien été nourris !");
+            $sender->addFood("20");
+        }
+        break;
+        }
+
+        return true;
+
+    }
+}
